90f47bbdf7455ff307fb1caa0e7a4a97489ba271
Satturnooo0 <theflashgss@gmail.com>
2023-09-12 21:32:58 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "Boruto de Volta a Vila da Folha!",
+        "description": "Terminou o arco de desaparecimento de Mitsuki e, no final, revelou qual a punição para o personagem por ter deixado a Vila da Folha. Na verdade, Sarada e Boruto também foram punidos. Os 3 ninjas perderam seus status de Genin (temporiariamente)!",
+        "photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2023/08/boruto_two_blue_vortex_personagens_estreia__2n503l-1210x544.jpg"
+    },
+    {
+        "id":"2",
+        "title": "New Designer da Sarada em Boruto Two Blue Vortex",
+        "description": "Seu novo design apresenta a garota com cabelos mais curtos, brincos que representam o Clã Uchiha e uma jaqueta preta com detalhes em vermelho, bem similar a que Boruto usava quando era mais novo. Outro detalhe é que as cores usadas pela personagem também remetem ao grupo criminoso mais famoso da franquia: a Akatsuki.",
+        "photoCover": "https://vocesabianime.com/wp-content/uploads/2023/07/Boruto-Two-Blue-Vorte-Sarada-Adulta-viraliza-nas-redes-sociais-1133x637-1.jpg"
+    },
+    {
+        "id":"3",
+        "title": "Gojo vs Sukuna Batalha Épica",
+        "description": "O capítulo 228 de Jujutsu Kaisen nos mostrou alguns feitos incríveis de Gojo e Sukuna. Eles superaram seus limites e se surpreenderam com sua astúcia e criatividade. Gojo usou suas habilidades Six Eyes e Infinity para manipular o espaço e o tempo. Sukuna liberou suas habilidades Malevolent Shrine e Cursed Finger para desencadear ataques devastadores.",
+        "photoCover":"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhprjb-4t-bVA8gl8ZLmOVUdp1FzvnVBEw-yHG6kyncjzWZlw97xntp6Y88rD67Z9SVTU1nKyTrc8R03j2xwpsyW7-xyOnTR9HkwUuV4k3-x0-1HhQR6fPoZr51FA7VNAIdS2xOQ1v4hlKq-S5NnGUzFm-2qAdGcU5HKEDP1v6ZA2gniAC5J5QZygTwrag/w640-h466/JJK%20229%20Gojo%20vs%20Sukuna.webp"
+    },
+    {
+        "id":"4",
+        "title": "Spy x Family ganha Trailer de Filme 'Spy x Family Code: White'",
+        "description": "Foi divulgado um trailer do filme SPY×FAMILY CODE: White, que estreia no Japão em 22 de dezembro, quando a segunda temporada estiver terminando o primeiro cour (caso haja mais de um).",
+        "photoCover":"https://www.jbox.com.br/wp/wp-content/uploads/2023/06/spy-x-family-filme-poster-destacada.jpg.avif"
+    }
+
+]
+  
